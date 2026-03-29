097ee6c58eaaf5eb1575fb2829ac14b53062af35
Arif Mahmud Pranto <pranto48@gmail.com>
2026-03-29 11:56:11 +0600

    Add SMTP mailer helper for authenticated TLS/SSL sending

--- docker-ampnm/api.php
+++ docker-ampnm/api.php
@@ -161,6 +161,9 @@ try {
     $metricsActions = [
         'get_latest_metrics', 'get_metrics_history', 'get_all_hosts',
         'get_agent_tokens', 'create_agent_token', 'delete_agent_token', 'toggle_agent_token',
+        'create_device_from_host', 'register_host_ip',
+        'get_alert_settings', 'save_alert_settings',
+        'get_host_override',
         'get_all_host_overrides', 'save_host_override', 'delete_host_override',
         'export_host_overrides', 'import_host_overrides',
     ];
