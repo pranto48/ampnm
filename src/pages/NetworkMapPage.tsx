8fa1bbff26fff0d8296ff324556793a00f69a938
[dyad] <pranto48@gmail.com>
2026-03-30 22:25:47 +0600

    Update 1 file

--- src/pages/NetworkMapPage.tsx
+++ src/pages/NetworkMapPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const NetworkMapPage: React.FC = () => {
   const history = useHistory();
