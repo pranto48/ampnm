1e70c84c99f9d4b82f21025529c17d9b5f553781
Arif Mahmud Pranto <pranto48@gmail.com>
2026-07-21 13:18:54 +0600

    chore: bump application version from v1.14 to v1.15 across backend and web documentation

--- update_status.php
+++ update_status.php
@@ -115,7 +115,7 @@ function getDockerHubUpdateStatus(): array {
         $httpCodeAll = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
         
-        $latestTag = 'v1.14';
+        $latestTag = 'v1.15';
         if ($httpCodeAll === 200 && $responseAll) {
             $dataAll = json_decode($responseAll, true);
             if (!empty($dataAll['results'])) {
@@ -129,7 +129,7 @@ function getDockerHubUpdateStatus(): array {
                 }
             }
         }
-        $currentTag = 'v1.14'; 
+        $currentTag = 'v1.15'; 
         $updateAvailable = ($latestTag !== $currentTag);
         $remoteDigest = $latestTag;
         $localDigest = $currentTag;
